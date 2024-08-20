00582b11075cba6b62c35c48de15cfc9a9cb7307
HenryAllen <henrique.goncalves1712@gmail.com>
2024-08-20 14:54:22 -0300

    Comentários e atualizções

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-let numero= Math.round(Math.random()*50 );
-console.log(numero);
+//Variaveis para o número aleatório, número de tentativas e o jogo estar com limite
+let numero;
 let tentativas = 3;
 let ativo = true;
 
+
+function inicio(params) {
+    numero= Math.round(Math.random()*50 );
+    tentativas = 3;
+    ativo = true;
+    document.getElementById("chances").innerHTML = `Você tem ${tentativas} tentativa(s)`;
+    
+}
+
+//Reiniciar o jogo quando acabar ou ganhar
 function reiniciar(params) {
     numero= Math.round(Math.random()*50 );
-    console.log(numero);
     tentativas = 3;
     ativo = true;
     document.getElementById("dica").innerHTML = "";
@@ -17,8 +26,9 @@ function reiniciar(params) {
 }
 
 
-
+//Função principal do jogo
 function jogar() {
+    //Variaveis das respostas do usuario, das dicas, da resposta de vitoria ou derrota e das chances
     let user = parseInt(document.getElementById("userChoice").value);
     let tip = document.getElementById("dica");
     let resposta = document.getElementById("resposta");
@@ -28,41 +38,43 @@ function jogar() {
     
     
     
-  
+    //Caso o usuário coloque maior que o número limite, o programa não irá responder
     if (user > 50) {
         resposta.innerHTML = "Inválido";
         }
 
-        
-        if (ativo) {
-            if (tentativas > 0) {
-                if (user <= 50) {
+        //Código do jogo
+        if (ativo) { //Para saber se o jogo está funcionando
+            if (tentativas > 0) { //Para saber se o usuario tem tentativas ainda
+                if (user <= 50) {//Caso ele coloque menos ou igual ao número maximo, o jogo funciona
                     if (user == numero) {
                         tentativas == 0;
                         resposta.innerHTML = "Você acertou o número!"
                         tip.innerHTML = "";
                         ativo = false;
                         
-                       
+                       //As regras estão no HTML
                     }else if (user > numero) {
                         tip.innerHTML = "Você está quente";
-                        tentativas--;
+                        tentativas--;//Toda vez que o usuário erra, ele perde uma tentativa
                         chances.innerHTML = `Você tem ${tentativas} tentativa(s) `;
-                        resposta.innerHTML = "";
+                        
 
 
                     }else if (user < numero) {
                         tip.innerHTML = "Você está frio";
                         tentativas--;
                         chances.innerHTML = `Você tem ${tentativas} tentativa(s) `;
-                        resposta.innerHTML = "";
+                        
 
 
                      }
 
                   }
 
-              } else if(tentativas == 0){
+
+              } 
+              if(tentativas == 0){
                 resposta.innerHTML = "Você não tem mais tentativas. O número era " + numero + ".";
                 tip.innerHTML = "";
                 ativo = false;
@@ -70,15 +82,5 @@ function jogar() {
                 
               }
         }
-           
-       
-           
-        
-          
-        
-    
-        
-        
-
 }
 
